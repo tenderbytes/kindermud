b2f932a7a21be86e73ac6b1d722dd836ec70d4b5
Danny Krainas <dkrainas87@gmail.com>
2020-11-01 20:33:15 -0500

    wip

--- pkg/mud/server.go
+++ pkg/mud/server.go
@@ -1,18 +1,20 @@
 package mud
 
 import (
+	"context"
 	"sync"
 
 	"github.com/reiver/go-telnet"
 )
 
 type Connection struct {
-
+	Context context.Context
 }
 
 type ServerOptions struct {
 	Controllers       []*Controller
 	DefaultController *Controller
+	Addr              string
 }
 
 type Server struct {
@@ -23,14 +25,25 @@ type Server struct {
 }
 
 func NewServer(opts *ServerOptions) *Server {
-	return &Server{
+	srv := &Server{
 		DefaultController: opts.DefaultController,
 		Controllers:       opts.Controllers[:],
 		Connections:       make([]*Connection, 0),
+		server: &telnet.Server{
+			Addr:    opts.Addr,
+			Handler: srv,
+		},
 	}
+
+	return srv
 }
 
 func (srv *Server) ListenAndServe() error {
-	handler := telnet.EchoHandler
-return telnet.ListenAndServe(":5555", handler)
+	return srv.server.ListenAndServe()
+}
+
+func (srv *Server) ServeTELNET(ctx Context, w Writer, r Reader) {
+	conn := &Connection{
+		Context: ctx,
+	}
 }
